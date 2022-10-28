db1e37656a7dd4258056b6cd2168f3201b9accf7
dagbouj hatem <dagboujhatem@gmail.com>
2022-10-28 15:17:42 +0100

    create home service

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get(`${environment.baseURL}/home/products`);
+  }
+}
