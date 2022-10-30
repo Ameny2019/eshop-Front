b9ccb3e59cfffd96b350ca78f737fba8f657918b
dagbouj hatem <dagboujhatem@gmail.com>
2022-10-30 10:55:10 +0100

    refactor products images

--- src/app/services/efleure.service.ts
+++ src/app/services/efleure.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class EfleureService {
   constructor(private http: HttpClient) { }
 
     getfleurs(){
-      return this.http.get(`http://localhost:3000/efleur/GetAllEfleur/`)
+      return this.http.get(`${environment.baseURL}/efleur/GetAllEfleur/`)
     }
 }
