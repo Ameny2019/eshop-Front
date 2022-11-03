1d8986dc0a2e2f32e1b45da3c6651ca08f6ca7b2
dagbouj hatem <dagboujhatem@gmail.com>
2022-11-03 19:24:51 +0100

    prod settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL : '',
-  stripeAPIKey: ''
+  baseURL : 'https://backend-post-tn.herokuapp.com',
+  stripeAPIKey: 'pk_test_51JdXf9DRYvhLYt7LKXzoEVUDlUQMXzXyDMHYKOEo0RRsLm6ZUYCfgPOaf6AH74OcyORolgD406J76HXvbnIPbLTD00XWoH3GaV'
 };
